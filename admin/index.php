bf8496e694c51681de6ff8bc88fb7fc2e0903d90
Pan <b_pan2@fanshawec.ca>
2019-01-22 15:29:37 -0500

    added admin dashboard page

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<?php 
+//TODO: login needed
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to your admin panel</title>
+</head>
+<body>
+	<h1>Admin Dashboard</h1>
+	<h3>Welcome [Username]</h3>
+	<p>This is the admin dashboard page</p>
+
+	<nav>
+		<ul>
+			<li><a href="#">Create User</a></li>
+			<li><a href="#">Edit User</a></li>
+			<li><a href="#">Delete User</a></li>
+			<li><a href="#">Sign Out</a></li>
+		</ul>
+	</nav>
+</body>
+</html>
